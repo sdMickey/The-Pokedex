63dc169f5fd8392dd0430540efd8eeb05bf54de1
sdMickey <sohamdahiya@gmail.com>
2023-09-02 21:37:14 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/sdmickey/the_pokedex_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sdmickey.the_pokedex_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:the_pokedex_app/pages/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
